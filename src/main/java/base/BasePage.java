813d4a67b940cfad1a40b1abdcafdfd5f0015ccb
bharti-arora-tech <bhartiarora.v@gmail.com>
2026-03-06 00:31:49 +0530

    Update BasePage.java

--- src/main/java/base/BasePage.java
+++ src/main/java/base/BasePage.java
@@ -1,1 +1,8 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
