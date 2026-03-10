28165ef99ed75246e586da909db8d8e9d6f4ec6a
bharti-arora-tech <bhartiarora.v@gmail.com>
2026-03-10 21:13:32 +0530

    Screenshot Utility

--- src/main/java/utils/ScreenshotUtil.java
+++ src/main/java/utils/ScreenshotUtil.java
@@ -1,1 +1,41 @@
+package utils;
 
+import drivers.DriverManager;
+import org.openqa.selenium.*;
+
+import java.io.File;
+import java.nio.file.Files;
+
+/*
+Captures screenshot whenever test fails
+and stores it for Extent Report.
+*/
+
+public class ScreenshotUtils {
+
+public static String capture(String name){
+
+try{
+
+TakesScreenshot ts=
+(TakesScreenshot)DriverManager.getDriver();
+
+File src=ts.getScreenshotAs(OutputType.FILE);
+
+String path="reports/"+name+".png";
+
+File dest=new File(path);
+
+Files.copy(src.toPath(),dest.toPath());
+
+return path;
+
+}catch(Exception e){
+
+return null;
+
+}
+
+}
+
+}
